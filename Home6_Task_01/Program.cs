a976bb6eb0e5f63487ea4a1a10474ebaa4a5128e
e.tairov <ev.tairov@yandex.ru>
2023-02-06 00:23:30 +0700

    Программа не законченаgit add .!

--- Home6_Task_01/Program.cs
+++ Home6_Task_01/Program.cs
@@ -5,5 +5,33 @@
 //1, -7, 567, 89, 223-> 3
 
 //вводим через один Enter (разбиваем через Split())
-//--------------------------------------------------
+//-------------------------------------------------- (НЕ ЗАКОНЧЕНО!!!)
 
+
+int GetNumber()
+{
+    int[] array = new int[5];
+    int i = 0;
+
+    for (i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите любое число:");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    Console.WriteLine("[{0}]", string.Join(", ", array));
+    return array[i];
+}
+//--------------------------------
+void CheckParity(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+         if (arr[i] > 0) count++;
+    }
+    Console.Write($"Количество четных чисел -> {count}");
+}
+
+//--------------------------------
+int resultGetNumber = GetNumber();
+//CheckParity();
